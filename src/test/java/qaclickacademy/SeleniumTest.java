d32defc6bd79f13f80afcd8feaf4bfb11c561d13
Abinash <mabinashkumar1002@gmail.com>
2020-06-13 20:34:59 +0530

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,15 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+import junit.framework.TestCase;
+
+public class SeleniumTest extends TestCase{
+	
+	@Test
+	public void test()
+	{
+		System.out.println("Test Selenium");
+	}
+
+}
